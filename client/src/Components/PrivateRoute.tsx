34382844683f8ea3eba346566dbfa7f031fbb3cf
Joy <j26122003@gmail.com>
2023-12-08 21:13:41 +0530

    Final Commit

--- client/src/Components/PrivateRoute.tsx
+++ client/src/Components/PrivateRoute.tsx
@@ -3,7 +3,7 @@ import { Outlet, Navigate } from "react-router-dom";
 const PrivateRoute = () => {
   const isUserLoggedIn = localStorage.getItem("userData") ? true : false;
     return isUserLoggedIn ? <Outlet /> : <Navigate to="/" state={{
-        redirectMessage: "You must be logged in to view this page",
+        redirectMessage: "You must enter their details before accessing the page",
     }} />;
 }
 
